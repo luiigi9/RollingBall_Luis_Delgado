fd078cc83f1f7cfde75b96a7f80b61b1d99571d6
luiigi9 <luisdelgadoc.09@gmail.com>
2024-10-03 21:20:50 +0200

    RigidBody

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody rbB;
 
     Vector3 direccionB = new Vector3 (0f, 0f, 0f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,8 +18,12 @@ public class Ball : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rbB.AddForce(new Vector3(0f, 1f, 0f) * 5, ForceMode.Impulse);
+        }
         direccionB.z = v;
         direccionB.x = h;
-        transform.Translate (direccionB.normalized * Time.deltaTime);
+        rbB.AddForce(direccionB.normalized * 2, ForceMode.Force);
+
     }
 }
